b880527806adbf732453c418e1ef39a448c7459d
awanish285 <45520212+awanish285@users.noreply.github.com>
2022-03-09 02:41:22 +0530

    Updating comment in main class

--- restweb/src/main/java/com/spring/restweb/RestwebApplication.java
+++ restweb/src/main/java/com/spring/restweb/RestwebApplication.java
@@ -3,6 +3,9 @@ package com.spring.restweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//This is the main class
+
 @SpringBootApplication
 public class RestwebApplication {
 
